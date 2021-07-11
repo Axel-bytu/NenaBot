263eb9c3e3627fcbacf63924367df2129180d7a0
Axel-bytu <86326762+Axel-bytu@users.noreply.github.com>
2021-07-11 11:17:33 -0500

    Update and rename settings/config.js to ajustes/config.js

--- ajustes/config.js
+++ ajustes/config.js
@@ -1,6 +1,6 @@
 const config = {
-        botName: 'ShanBot',
-        ownerName: 'Shan',
+        botName: 'bytuSB',
+        ownerName: 'Axel-bytu',
         youtube: 'YOUTUBE_LINK',
         instagram: 'INSTAGRAM_LINK',
 }
