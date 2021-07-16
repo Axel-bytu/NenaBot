a58177f3a9c3f51e62fa7204edb516c8469fc0a4
Axel-bytu <86326762+Axel-bytu@users.noreply.github.com>
2021-07-16 16:29:45 -0500

    Update juegos.js

--- src/juegos.js
+++ src/juegos.js
@@ -4,12 +4,19 @@ const juegos = (prefix, pushname) => {
  
 ${prefix}gay
 Conoce que tan gay eres 😎
+
 ${prefix}cuties
 Prueba tu suerte en el medidor de fan de cuties 😱
 
+${prefix}rankgay
+conoce 5 tog de los màs gay del grupo
+
+${prefix}rankcornu
+conose 5 tog de los más cachudos del grupo
+
 _*En futuras versiones colocare mas juegos :)*_
      
-by shanduy`
+by ⚡ABT⚡`
 
 }
 
