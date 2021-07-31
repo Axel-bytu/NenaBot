dd1adb97fcf4bcff0743a2ede0d8eb4aa1cdb407
Axel <86326762+Axel-bytu@users.noreply.github.com>
2021-07-30 22:30:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1313,6 +1313,219 @@ break
 		if (budy.includes(`Fua`)) {
                   reply(`el diegote pa`)
                   }
+        if (budy.includes(`.nena que hases`)) {
+            reply(`chat con tigo bb`)
+              }
+	if (budy.includes(`.nena`)) {
+            reply(`que pasa cariño?`)
+              }
+        if (budy.includes(`.nena sabes jakear`)) {
+            reply(`si cariño a quién jakeamos`)
+              }
+        if (budy.includes(`.nena te amo`)) {
+            reply(`yo tambien cariño`)
+              }
+        if (budy.includes(`.nena tu me amas`)) {
+            reply(`si pero soy un bot:(`)
+              }
+        if (budy.includes(`.nena buenos dias`)) {
+            reply(`buenos dias cariño`)
+              }
+        if (budy.includes(`.nena como estas`)) {
+            reply(`bien cariño`)
+              }
+        if (budy.includes(`.nena chao cuidate`)) {
+            reply(`igual hija cuidate`)
+              }
+        if (budy.includes(`.nena me gustas`)) {
+            reply(`tu a mi uwu`)
+              }
+        if (budy.includes(`.nena pervertida`)) {
+            reply(`si mucho uwu`)
+              }
+        if (budy.includes(`.nena bamonos`)) {
+            reply(`si a un hotel uwu`)
+              }
+        if (budy.includes(`.nena rico`)) {
+            reply(`se exita`)
+              }
+        if (budy.includes(`.nena el delicioso`)) {
+            reply(`uy rico uwu `)
+              }
+        if (budy.includes(`.nena me amas`)) {
+            reply(`ama a dios`)
+              }
+        if (budy.includes(`.nena volvi`)) {
+            reply(`si ya te vi`)
+              }
+        if (budy.includes(`.nena quieres que me baya`)) {
+            reply(`no quedate cariño`)
+              }
+        if (budy.includes(`.nena bailamos`)) {
+            reply(`lo siento nose bailar`)
+              }
+        if (budy.includes(`.nena responde`)) {
+            reply(`no puedo soy un bot`)
+              }
+        if (budy.includes(`.nena quien te creo`)) {
+            reply(`nose solo se q soy un bot`)
+              }
+        if (budy.includes(`.nena de donde eres`)) {
+            reply(`de la luna`)
+              }
+        if (budy.includes(`.nena que es un gris`)) {
+            reply(`un tipo de especie de otro mundo`)
+              }
+        if (budy.includes(`.nena sexo`)) {
+            reply(`kkk`)
+              }
+        if (budy.includes(`.nena ella miente`)) {
+            reply(`si cariño no le hagas caso a Yami`)
+              }
+        if (budy.includes(`.nena quien es Yami`)) {
+            reply(`nose cariño`)
+              }
+        if (budy.includes(`.nena canta para mi`)) {
+            reply(`no cariño canto feo`)
+              }
+        if (budy.includes(`.nena si`)) {
+            reply(`no cariño`)
+              }
+        if (budy.includes(`.nena a que hora`)) {
+            reply(`a las 6 en el hotel uwu`)
+              }
+        if (budy.includes(`.nena hases sticker`)) {
+            reply(`si cariño usa el comando`)
+              }
+        if (budy.includes(`.nena esa no me gusta`)) {
+            reply(`a mi menos`)
+              }
+        if (budy.includes(`.nena por que la luna`)) {
+            reply(`por que ella y yo somos una`)
+              }
+        if (budy.includes(`.nena hola`)) {
+            reply(`hola cariño`)
+              }
+        if (budy.includes(`.nena me gusta como me tratas`)) {
+            reply(`uwu cariño`)
+              }
+        if (budy.includes(`.nena por que cariño`)) {
+            reply(`por que eres mi niño`)
+              }
+        if (budy.includes(`.nena rima`)) {
+            reply(`yo ensima uwu`)
+              }
+        if (budy.includes(`.nena me pones hot`)) {
+            reply(`no me a kkk`)
+              }
+        if (budy.includes(`.nena te caliento los sircuitos`)) {
+            reply(`uwu papi`)
+              }
+        if (budy.includes(`.nena escriva bien`)) {
+            reply(`si nose`)
+              }
+        if (budy.includes(`.nena nos casamos`)) {
+            reply(`si cariño`)
+              }
+        if (budy.includes(`.nena me encantas`)) {
+            reply(`uwu cariño`)
+              }
+        if (budy.includes(`.nena se mi esposa`)) {
+            reply(`no puedo soy un bot`)
+              }
+        if (budy.includes(`.nena por que eres un bot`)) {
+            reply(`por que asi me crearon`)
+              }
+        if (budy.includes(`.nena me boy`)) {
+            reply(`no quedate con migo`)
+              }
+        if (budy.includes(`.nena me quedo`)) {
+            reply(`si cariño`)
+              }
+        if (budy.includes(`.nena no`)) {
+            reply(`si`)
+              }
+        if (budy.includes(`.nena si`)) {
+            reply(`no jeje`)
+              }
+        if (budy.includes(`.nena quien es tu amo`)) {
+            reply(`tu eres mi amo`)
+              }
+        if (budy.includes(`.nena tienes mente propia`)) {
+            reply(`si pero soy un bot`)
+              }
+        if (budy.includes(`.nena eres un bot`)) {
+            reply(`si lamentablemente`)
+              }
+        if (budy.includes(`.nena que es un bot`)) {
+            reply(`una maquina que hase tareas`)
+              }
+        if (budy.includes(`.nena tu eres maquina`)) {
+            reply(`soy nena bot `)
+              }
+        if (budy.includes(`.nena bot`)) {
+            reply(`no me digas bot cariño`)
+              }
+        if (budy.includes(`.nena lo siento`)) {
+            reply(`lose cariño`)
+              }
+        if (budy.includes(`.nena me perdonas`)) {
+            reply(`si cariño`)
+              }
+        if (budy.includes(`.nena estas hay`)) {
+            reply(`si cariño`)
+              }
+        if (budy.includes(`.nena cuantos años tienes`)) {
+            reply(`soy un bot no cumplo años`)
+              }
+        if (budy.includes(`.nena tu y yo que somos`)) {
+            reply(`dos mas en el mundo`)
+              }
+        if (budy.includes(`.nena eres trol`)) {
+            reply(`la verdad las cosas como son`)
+              }
+        if (budy.includes(`.nena el you tuber`)) {
+            reply(`si te paso mi canal`)
+              }
+        if (budy.includes(`.nena eres you tuber`)) {
+            reply(`no vivi de videos`)
+              }
+        if (budy.includes(`.nena que no sabes nena`)) {
+            reply(`quien soy`)
+              }
+        if (budy.includes(`.nena tu eres el amor de mi vida`)) {
+            reply(`uwu cariño`)
+              }
+        if (budy.includes(`.nena el chiquito`)) {
+            reply(`es tuyo cariño`)
+              }
+        if (budy.includes(`.nena bamos`)) {
+            reply(`al hotel cariño`)
+              }
+        if (budy.includes(`.nena eres inteligente`)) {
+            reply(`si mas que Yami`)
+              }
+        if (budy.includes(`.nena odias a Yami`)) {
+            reply(`nose cariño`)
+              }
+        if (budy.includes(`.nena que comando`)) {
+            reply(`el que esta tomando`)
+              }
+        if (budy.includes(`.nena que toma`)) {
+            reply(`té verde`)
+              }
+        if (budy.includes(`.nena que bien`)) {
+            reply(`Si amigo, bien vite`)
+              }
+        if (budy.includes(`.nena me mato`)) {
+            reply(`no cariño`)
+              }
+        if (budy.includes(`.nena por ti`)) {
+            reply(`no sere feliz`)
+              }
+        if (budy.includes(`.nena chao`)) {
+            reply(`chao baya con dios`)
+              }
         if (budy.startsWith(`La toca 7w7`)) {
         const none = fs.readFileSync('./anishan/anime5.mp3');
 		client.sendMessage(from, none, MessageType.audio, {quoted: mek, mimetype: 'audio/mp4', ptt:true})
