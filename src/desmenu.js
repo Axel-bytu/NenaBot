f2f11a61e654756bc7e66cd0da427e3532c7bf67
Axel <86326762+Axel-bytu@users.noreply.github.com>
2021-08-03 10:58:22 -0500

    Update desmenu.js

--- src/desmenu.js
+++ src/desmenu.js
@@ -18,7 +18,7 @@ Coloca el link del video YT
     
 _*play y *play2 tiene un limite de 2000 canciones en el server, si llega a su limite se restablecera dentro de 24 horas_
      
-by ⚡ABT⚡`
+by 𝐍𝐞𝐧𝐚𝐁𝐨𝐭ꨄ︎`
 
 }
 
