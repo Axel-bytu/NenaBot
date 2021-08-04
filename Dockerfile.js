0beb80a98dff99386622b6ad53c33fcd957439c1
Axel <86326762+Axel-bytu@users.noreply.github.com>
2021-08-03 19:18:11 -0500

    Update Dockerfile.js

--- Dockerfile.js
+++ Dockerfile.js
@@ -1,8 +1,8 @@
 FROM shanduy/shanBot:latest
 
 RUN git clone https://github.com/Axel-bytu/NenaBot /root/NenaBot
-WORKDIR /root/NenaBot/
-ENV EC = Americadelsur/Ecuador
+WORKDIR /root/nenabot/
+ENV EC = americadelsur/ecuador
 RUN npm install supervisor -g
 RUN yarn install --no-audit
 
