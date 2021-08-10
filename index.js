d6ad8a13ee6df71823d563eb544dbfcfc2ff1e9e
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2021-08-09 23:28:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1532,6 +1532,10 @@ break
         if (budy.includes(`.nena funalo`)) {
             reply(`ok cariño `)
               }
+                if (budy.startsWith(`.menu`)) {
+        const none = fs.readFileSync('./anishan/menu.mp3');
+		client.sendMessage(from, none, MessageType.audio, {quoted: mek, mimetype: 'audio/mp4', ptt:true})
+                  }
         if (budy.startsWith(`La toca 7w7`)) {
         const none = fs.readFileSync('./anishan/anime5.mp3');
 		client.sendMessage(from, none, MessageType.audio, {quoted: mek, mimetype: 'audio/mp4', ptt:true})
