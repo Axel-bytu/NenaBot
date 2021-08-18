667f143bb3fedb591774be50426000c2448d832e
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2021-08-17 23:46:08 -0500

    Update logomenu.js

--- src/logomenu.js
+++ src/logomenu.js
@@ -1,1 +1,29 @@
+const logomenu = (prefix, pushname) => {
+    return `
+ comandos de logos 
+ 
+ 🌀Use prefijo mas comando y texto🌀
 
+╭➽${prefix}neon
+├➽${prefix}matrix
+├➽${prefix}break
+├➽${prefix}dropwater
+├➽${prefix}lobo
+├➽${prefix}flores
+├➽${prefix}cross
+├➽${prefix}seda
+├➽${prefix}fire
+├➽${prefix}glow
+├➽${prefix}smoke
+├➽${prefix}pubg
+├➽${prefix}cielo
+├➽${prefix}cs
+├➽${prefix}ligth
+├➽${prefix}navidad
+╰➽${prefix}nieve
+
+by 𝐍𝐞𝐧𝐚𝐁𝐨𝐭ꨄ︎`
+
+}
+
+exports.logomenu = logomenu
