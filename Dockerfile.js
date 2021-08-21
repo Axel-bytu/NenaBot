e1b332b4d699228ad22480625f3d01710b4bc0e2
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2021-08-20 20:23:23 -0500

    Delete Dockerfile.js

--- Dockerfile.js
+++
@@ -1,9 +0,0 @@
-FROM shanduy/shanBot:latest
-
-RUN git clone https://github.com/Axel-bytu/NenaBot /root/NenaBot
-WORKDIR /root/nenabot/
-ENV EC = americadelsur/ecuador
-RUN npm install supervisor -g
-RUN yarn install --no-audit
-
-CMD ["node", "index.js"]
