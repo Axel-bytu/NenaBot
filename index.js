d5624548612c4879925ba0212165af2fbfc3b5fa
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2021-08-23 21:57:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -510,9 +510,9 @@ message: { "imageMessage": { "mimetype": "image/jpeg","caption": `🥀Axel y Fer
 			switch(command) {
 		case 'help':
 		case 'menu':   
-                const fimg = fs.readFileSync('./src/nenabot.jpg');
+               /*const fimg = fs.readFileSync('./src/nenabot.jpg');*/
 		client.sendMessage(from, fimg(jpg), MessageType.jpg, {quoted: mek, mimetype: 'image/jpg', ptt:true})                
-                client.sendMessage(from, help(prefix, jpg, pushname, date, time, sender), text, {quoted: mek})
+                client.sendMessage(from, help(prefix, fs, pushname, date, time, sender), text, {quoted: mek})
                 break
                 case 'otak':
 		client.sendMessage(from, otak(prefix, sender), text, {quoted: mek})
