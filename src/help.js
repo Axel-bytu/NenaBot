7d447f57954ced594cb8064b0bda7b5ad60169e4
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2021-08-23 22:16:28 -0500

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 const fimg = fs.readFileSync('./src/nenabot.jpg');
-const help = (prefix, fimg, pushname, date, time) => {
+const help = (prefix, fs, pushname, date, time) => {
       return `
 
 ┏ ❥︎*𝐍𝐄𝐍𝐀𝐁𝐎𝐓 by 𝐁𝐑𝐎𝐙*❦︎
-╿${fimg}
+╿${fs}
 ┷┯*INFORMACION*
   ╽
   ┠❥ Hola: ${pushname}
