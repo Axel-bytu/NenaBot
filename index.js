a8110e30387e103e26e5d3e09c9397284111c7c7
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2021-08-25 12:57:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -504,7 +504,8 @@ message: { "imageMessage": { "mimetype": "image/jpeg","caption": `🥀Axel y Fer
 			switch(command) {
 		case 'help':
 		case 'menu':                   
-                client.sendMessage(from, help(prefix, fimg, pushname, time, date, sender), text, {quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "mimetype": "image/jpeg", "caption": "➫𝑴𝒆𝒏𝒖 𝑫𝒆 𝑵𝒂𝒏𝒂𝑩𝒐𝒕\n❣️⃞🔥𝘼꯭𝙭͠𝙚꯭𝙡 𝙮  ꯭𝙁𝙚𝙧꯭𝙣𝙖꯭𝙣𝙙꯭𝙖🔥❣️" ,"jpegThumbnail": fs.readFileSync(`./src/nenabot.jpg`)}}}})
+                const fimg = { "mimetype": "image/jpeg","caption": `🥀Axel y Fernanda`, 'jpegThumbnail': fs.readFileSync('./src/nenabot.jpg')}
+                client.sendMessage(from, help(prefix, fimg, pushname, time, date, sender), text, {quoted: mek})
                 break
                 case 'otak':
 		client.sendMessage(from, otak(prefix, sender), text, {quoted: mek})
