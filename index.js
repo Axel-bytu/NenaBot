1d2a686bc09f0c53078de4c5b34be3f16e16ff2f
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2021-08-24 22:01:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -46,8 +46,6 @@ const lolis = require('lolis.life')
 const loli = new lolis()
 const speed = require('performance-now')
 const chalk = require('chalk')
-const crypto = require("crypto-js")
-const CryptoJS = require("crypto-js")
 /******FIN DE ENTRADA DEL PAQUETE NPM******/
 
 /******COMIENZO DE LA ENTRADA JSON******/
