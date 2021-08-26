111058fc65a0d5d5b25546d23f98392c19559214
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2021-08-25 19:10:43 -0500

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,7 +1,6 @@
 const fs = require('fs')
 const fimg = fs.readFileSync('./src/nenabot.jpg');
-const help = (prefix, fimg, pushname, time, date) => {
-client.sendMessage(from, fimg, MessageType.image, {quoted: mek, mimetype: 'image/jpg', ptt:true})      
+const help = (prefix, fimg, pushname, time, date) => {      
        return `
 
 ┏ ❣️*𝐍𝐄𝐍𝐀𝐁𝐎𝐓 by 𝐁𝐑𝐎𝐙*❣️
